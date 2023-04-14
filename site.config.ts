4403aef89149cdea0be55aff4fa7f83fadb39e4e
maulanarizki27 <124870790+maulanarizki27@users.noreply.github.com>
2023-04-14 15:23:05 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'fec569920e2749ae8d51b5e3d7dd703c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'maulana rizki',
+  domain: 'maulanaunimed.eu.org',
+  author: 'maulana rizki',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
